dc9facc3e8ffd08ac372b188d92b4adc2f6416a4
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-02-14 11:24:33 +0000

    tested computeWordEntropy method

--- src/test/java/com/smola/words/WordsEntropyTest.java
+++ src/test/java/com/smola/words/WordsEntropyTest.java
@@ -2,6 +2,12 @@ package com.smola.words;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordsEntropyTest {
@@ -21,5 +27,28 @@ public class WordsEntropyTest {
         // Then
         double expected = 3;
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testComputeWordEntropy(){
+        // Given
+        List<String> testWordsList = new ArrayList<>();
+        testWordsList.add("birds");
+        testWordsList.add("troll");
+        testWordsList.add("anger");
+        testWordsList.add("bonds");
+        LinkedHashMap<String, Double> testProbabilities = new LinkedHashMap<>();
+        for (int i = 0; i < testWordsList.size(); i++) {
+            testProbabilities.put(testWordsList.get(i), 0.25);
+        }
+        Words testWords = new Words();
+        testWords.setPossibleGuesses(testWordsList);
+        testWords.setProbabilityDistribution(testProbabilities);
+        // When
+        double actual = underTest.computeWordEntropy("birds", testWords);
+        // Then
+        double expected = 1.5;
+        assertThat(actual).isEqualTo(expected);
+
     }
 }
