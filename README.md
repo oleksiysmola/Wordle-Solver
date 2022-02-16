e6af577650d406ad96f5f4cd5fe6533f2c6f4fa6
Oleksiy Smola <oleksiy.smola@gmail.com>
2022-02-16 23:02:20 +0000

    added CLI to Main to enable a user to input guesses and receive suggestions

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ solver based on concepts in information theory
 utilised in a YouTube video by 3Blue1Brown
 - Link: https://www.youtube.com/watch?v=v68zYyaEmEA
 - Possible Words: https://gist.github.com/Brystephor/c7fde59e673534dcb4d687243195b544
+## How to use
+The Wordle solver is run through the Main java class.
+Note that the system only suggests guesses; it does
+not play the game for you. A command line interface
+prompts the user to input various responses that 
+indicate their guesses and the patterns they find while
+playing the game. Based on these prompts one should answer 
+as follows:
+- Y/N: Y for if you wish to proceed or N if you wish to abort
+- What word did you guess? This should be the five letter word
+you just entered into Wordle
+- What pattern did you obtain? If the below pattern is obtained
+from a guess then the input should be "f,1 o,2 d,0 s4,1".
+In general this denotes first the letter then how many times it
+occurs in the answer. Here f occurs once so f,1 represents this,
+the two occurrences of o are denoted by o,2 and the lack of any d 
+in the answer is indicated by d,0. We treat greens separately and
+the letter is proceeded by the index denoting its position in the word.
+Here s4,1 specifies the existence of an s specifically at index 4
+in the answer.
+  ![alt Example of possible Wordle pattern](patternexample1.png "Title")
+For the second example, we consider instead case where the second
+o is highlighted green. The input pattern would then be
+"f,1 o,1 d,0 o2,1 s4,1".
+  ![alt Example of possible Wordle pattern](patternexample2.png "Title")
--- src/main/java/com/smola/words/Main.java
+++ src/main/java/com/smola/words/Main.java
@@ -1,25 +1,101 @@
 package com.smola.words;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         // Create instance of WordsFiles class
         WordsFiles wordsFiles = new WordsFiles();
         // Generate new words object
-        Words initialWords = new Words();
+        Words words = new Words();
         // Get initial words
-        List<String> initialWordsList = new ArrayList<>();
         File initialGuessFile = new File("initialentropy.txt");
-        initialWords = wordsFiles.readWordsEntropyFromFile(initialGuessFile, initialWords);
-        initialWords.setPossibleGuesses(initialWordsList);
+        words = wordsFiles.readWordsEntropyFromFile(initialGuessFile, words);
         // Set to uniform distribution
         WordsProbabilities wordsProbabilities = new WordsProbabilities();
-        wordsProbabilities.generateUniformDistribution(initialWords);
+        wordsProbabilities.generateUniformDistribution(words);
         // Get entropy distribution
         WordsEntropy wordsEntropy = new WordsEntropy();
         WordsPattern wordsPattern = new WordsPattern();
+
+        // CLI
+        System.out.println("Welcome to Wordle Solver Version 1!");
+        System.out.println("------------------------------------");
+        System.out.println("Would you like to play a game? Y/N");
+        // Start up scanner
+        Scanner scanner = new Scanner(System.in);
+        String userResponse;
+        // List and map for current guess pool
+        List<String> currentGuesses;
+        LinkedHashMap<String, Double> currentEntropy;
+        // Store best guess and entropy
+        Double highestEntropy = 0.0;
+        String bestGuess = "";
+        // Store word guessed
+        String wordGuessed;
+        // For pattern input
+        String patternInput;
+        String[] patternSplit;
+        String[] patternKeyValuePairs;
+        LinkedHashMap<String, Integer> processedPattern;
+        // Boolean to track if game is running
+        boolean isRunning = true;
+        // Loop while running
+        while (isRunning){
+            userResponse = scanner.nextLine();
+            switch (userResponse){
+                case "Y":
+                    // Obtain current word and entropy pool
+                    currentGuesses = words.getPossibleGuesses();
+                    currentEntropy = words.getEntropyDistribution();
+                    System.out.println("Computing best guess...");
+                    // Reset highest entropy
+                    highestEntropy = 0.0;
+                    // Loop through all available guess
+                    for (String word: currentGuesses){
+                        // Compares to current highest entropy
+                        if (currentEntropy.get(word) >= highestEntropy){
+                            bestGuess = word;
+                            highestEntropy = currentEntropy.get(word);
+                        }
+                    }
+                    System.out.println("Your best guess would be " + bestGuess
+                    + " which maximises entropy at " + highestEntropy + " bits!");
+                    System.out.println(" ");
+                    // If we win time to wrap up
+                    System.out.println("Did the guess succeed? Y/N");
+                    userResponse = scanner.nextLine();
+                    if (userResponse.equals("Y")){
+                        System.out.println("Congratulations!");
+                        isRunning = false;
+                        break;
+                    }
+                    System.out.println("Please enter your guess:");
+                    wordGuessed = scanner.nextLine();
+                    System.out.println("Please enter the pattern you obtained:");
+                    patternInput = scanner.nextLine();
+                    // Processes input pattern
+                    patternSplit = patternInput.split(" ");
+                    processedPattern = new LinkedHashMap<>();
+                    for (int i = 0; i < patternSplit.length; i++){
+                        patternKeyValuePairs = patternSplit[i].split(",");
+                        processedPattern.put(patternKeyValuePairs[0],
+                                Integer.valueOf(patternKeyValuePairs[1]));
+                    }
+                    System.out.println("Reducing words list...");
+                    // Filter words that do not match and recompute probabilities and entropy
+                    words = wordsPattern.findMatchingWords(wordGuessed, words, processedPattern);
+                    words = wordsProbabilities.generateUniformDistribution(words);
+                    words = wordsEntropy.obtainEntropyDistribution(words, wordsPattern, wordsProbabilities);
+                    System.out.println("Do you wish to continue playing? Y/N");
+                    break;
+                case "N":
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Sorry, please enter Y/N");
+            }
+        }
     }
 }
